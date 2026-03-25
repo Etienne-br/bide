ac8dc6464333df6beb10ebafcec9336d5d8942b2
Gaspard-f0urc <gaspard.fourcassie@esme.fr>
2026-03-25 11:25:20 +0100

    update main

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-
- bjbj
+#bid_cost(price) = base_cost + α/(price+1) 
+def alpha(k):
+    if k==0:
+        return False
+    return 100/k
+def bid_cost(price):
+    if price==0:
+        return f"error cannot put 0"
+    return base_cost+alpha(k)/(price+1)
+base_cost=10
+k=1
+print(bid_cost(10))
